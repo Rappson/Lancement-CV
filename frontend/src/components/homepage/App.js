039f9ea050070e6b1bda4633e1e4ef642f7447e3
rappson <axel.grolier@gmail.com>
2023-07-13 11:56:17 +0200

    mise en place du Header

---
+++ frontend/src/components/homepage/App.js
@@ -0,0 +1,20 @@
+import '../../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='title'>Mon super CV</h1>
+      </header>
+      <div className='main'>
+        <div className='sidebar'>
+          <ul>
+            <li>Présentation</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
