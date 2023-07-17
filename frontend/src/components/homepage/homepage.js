aab5127d357f380b7705be53a1bf961c39e1d48f
Axel Grolier <69462172+Rappson@users.noreply.github.com>
2023-07-17 21:08:13 +0200

    mise en place du component du contenu

---
+++ frontend/src/components/homepage/homepage.js
@@ -0,0 +1,14 @@
+import '../../style/App.css';
+
+function Homepage() {
+    return (
+        <div className='Homepage d-flex justify-content-center'>
+            <div className='content d-flex w-50'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis minus nostrum quisquam sapiente officiis itaque sint eveniet,
+                     illo nisi adipisci eum magnam vitae minima obcaecati hic veniam nihil placeat quaerat.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Homepage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
-import Header from './components/homepage/Header';
+import Homepage from './components/homepage/homepage';
+import Header from './components/global-components/Header';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    <Header/>
+    <Homepage />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
