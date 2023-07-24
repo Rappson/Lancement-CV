1fd6d1e298bac3c31e3d85b183a52465a12233ce
Axel Grolier <69462172+Rappson@users.noreply.github.com>
2023-07-24 13:24:49 +0200

    positionnement des blocs dans le footer

--- frontend/src/components/global-components/Footer.js
+++ frontend/src/components/global-components/Footer.js
@@ -1,11 +1,16 @@
 import '../../style/App.css'
 
-function Footer (){
+function Footer() {
     return (
         <footer>
-            <div className="contact">
-                <p>Mail</p>
+            <div className="container contact row">
+                <div className='my_information col-3'>
+                    <p>Mail</p>
+                </div>
+
+                <div className="jobs_informations col-3">
                 <p>Ancien job (adresse, tel, mail)</p>
+                </div>
 
             </div>
         </footer>
