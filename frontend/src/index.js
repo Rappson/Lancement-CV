f9ed760ce68a067b5471bb2dbbfdef9b32ca3cd8
Axel Grolier <69462172+Rappson@users.noreply.github.com>
2023-07-24 12:47:12 +0200

    Ajout du footer dans le compenennt de base

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import Homepage from './components/homepage/homepage';
 import Header from './components/global-components/Header';
+import Footer from './components/global-components/Footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,5 +11,6 @@ root.render(
   <React.StrictMode>
     <Header/>
     <Homepage />
+    <Footer />
   </React.StrictMode>
 );
