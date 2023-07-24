2a0288cc8c7e4ad39587e595c6dd41228f10a61c
Axel Grolier <69462172+Rappson@users.noreply.github.com>
2023-07-24 12:41:13 +0200

    Mise en pace du footer

---
+++ frontend/src/components/global-components/Footer.js
@@ -0,0 +1,15 @@
+import '../../style/App.css'
+
+function Footer (){
+    return (
+        <footer>
+            <div className="contact">
+                <p>Mail</p>
+                <p>Ancien job (adresse, tel, mail)</p>
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
