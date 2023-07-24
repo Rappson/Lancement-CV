aace524ffb6fc26d4030dc41f9c580e69635dd89
Axel Grolier <69462172+Rappson@users.noreply.github.com>
2023-07-24 13:15:38 +0200

    correction afin de centrer la liste dans le header et mise en propre suite aux changements

--- frontend/src/components/global-components/Header.js
+++ frontend/src/components/global-components/Header.js
@@ -6,13 +6,13 @@ function Header() {
       <div className="title-container">
         <h1 className='title'>Mon super site</h1>
       </div>
-      <div className='main'>
-        <div className='sidebar'>
-          <ul className="main-menu d-flex flex-row justify-content-evenly">
-            <li>Présentation</li>
-            <li>Expériences</li>
-            <li>A propos de moi</li>
-            <li>Commentaires</li>
+      <div className='container'>
+        <div className='sidebar row'>
+          <ul className="main-menu row">
+            <li className='col-3'>Présentation</li>
+            <li className='col-3'>Expériences</li>
+            <li className='col-3'>A propos de moi</li>
+            <li className='col-3'>Commentaires</li>
           </ul>
         </div>
       </div>
